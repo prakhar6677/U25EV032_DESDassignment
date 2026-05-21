6b4954eea5ad269e5addd42b1fc3cb2fa125ac3c
prakhar6677 <prakharverma397@gmail.com>
2026-05-21 22:55:17 +0530

    Added 2 bit Comp Cont assign testbench

---
+++ Assignment_1/tb_comparator_2bit.v
@@ -0,0 +1,35 @@
+module tb_comparator_dataflow;
+
+reg [1:0] A, B;
+wire A_greater, A_equal, A_less;
+
+comparator_dataflow uut (
+    .A(A),
+    .B(B),
+    .A_greater(A_greater),
+    .A_equal(A_equal),
+    .A_less(A_less)
+);
+
+initial begin
+
+    $dumpfile("comparator_dataflow.vcd");
+    $dumpvars;
+
+    A = 2'b00; B = 2'b00;
+    #10;
+
+    A = 2'b01; B = 2'b00;
+    #10;
+
+    A = 2'b10; B = 2'b11;
+    #10;
+
+    A = 2'b11; B = 2'b01;
+    #10;
+
+    $finish;
+
+end
+
+endmodule
