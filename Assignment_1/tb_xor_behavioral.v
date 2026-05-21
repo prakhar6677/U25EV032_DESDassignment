78c1442c42c8f2ff8634e2ad8f46045d2e2139a4
prakhar6677 <prakharverma397@gmail.com>
2026-05-21 22:31:28 +0530

    Added XOR behavioral Testbench

---
+++ Assignment_1/tb_xor_behavioral.v
@@ -0,0 +1,31 @@
+module tb_xor_behavioral;
+
+reg a, b;
+wire y;
+
+xor_behavioral uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("xor_behavioral.vcd");
+    $dumpvars(0, tb_xor_behavioral);
+
+    a = 0; b = 0;
+    #10;
+
+    a = 0; b = 1;
+    #10;
+
+    a = 1; b = 0;
+    #10;
+
+    a = 1; b = 1;
+    #10;
+
+    $finish;
+end
+
+endmodule
