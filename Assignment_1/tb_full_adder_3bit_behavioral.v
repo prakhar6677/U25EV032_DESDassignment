6c6d6eb92fe4e314e2880361b60ade6f833d7c07
prakhar6677 <prakharverma397@gmail.com>
2026-05-22 18:01:07 +0530

    Added 3 bit full adder behavioral testbench

---
+++ Assignment_1/tb_full_adder_3bit_behavioral.v
@@ -0,0 +1,31 @@
+module tb_adder_3bit_behavioral;
+
+  reg  [2:0] a, b;
+  reg  cin;
+  wire [2:0] sum;
+  wire cout;
+
+  adder_3bit_behavioral uut (
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+  );
+
+  initial begin
+    $dumpfile("output.vcd");
+    $dumpvars;
+  end
+
+  initial begin
+    a = 3'b000; b = 3'b000; cin = 0; #10;  // 0+0+0 = 0
+    a = 3'b001; b = 3'b001; cin = 0; #10;  // 1+1   = 2
+    a = 3'b011; b = 3'b010; cin = 0; #10;  // 3+2   = 5
+    a = 3'b101; b = 3'b010; cin = 1; #10;  // 5+2+1 = 8
+    a = 3'b111; b = 3'b001; cin = 0; #10;  // 7+1   = 8
+    a = 3'b111; b = 3'b111; cin = 1; #10;  // 7+7+1 = 15
+    $finish;
+  end
+
+endmodule
