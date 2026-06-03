a75f32e4b3bf70ef84ec8b7ecf32c16e9ec5240f
prakhar6677 <prakharverma397@gmail.com>
2026-06-03 15:12:53 +0530

    Added 2X4 decoder design

---
+++ decoder_(2X4).v
@@ -0,0 +1,15 @@
+module decoder2x4(
+    input [1:0] a,
+    output reg [3:0] y
+);
+
+always @(*) begin
+    case(a)
+        2'b00: y = 4'b0001;
+        2'b01: y = 4'b0010;
+        2'b10: y = 4'b0100;
+        2'b11: y = 4'b1000;
+    endcase
+end
+
+endmodule
